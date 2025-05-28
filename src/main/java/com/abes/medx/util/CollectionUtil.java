e616d8b72ef205402ce2e7bfa95c7c975eda3137
ujjwal0903 <ujjwalsinghal0903@gmail.com>
2025-05-28 09:59:50 +0530

    CollectionUtil added

--- src/main/java/com/abes/medx/util/CollectionUtil.java
+++ src/main/java/com/abes/medx/util/CollectionUtil.java
@@ -12,5 +12,7 @@ public class CollectionUtil {
         static {
             // Static Patient Data
             patientMap.put(1, new Patient("Raj", "Verma", "raj@gmail.com", "1234567890", "20", "P001"));
+            patientMap.put(2, new Patient("Yatharth", "Yadav", "yath@gmail.com", "1234599990", "21", "P002"));
+
         }
 }
